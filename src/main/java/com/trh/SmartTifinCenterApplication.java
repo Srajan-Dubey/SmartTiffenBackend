6c698e6226b28964ab18343247895b2e482698ac
Srajan-Dubey <srajandubey27@gmail.com>
2023-04-29 15:08:33 +0530

    first commit

---
+++ src/main/java/com/trh/SmartTifinCenterApplication.java
@@ -0,0 +1,13 @@
+package com.trh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTifinCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTifinCenterApplication.class, args);
+	}
+
+}
